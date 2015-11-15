f4cda6624d028fdabdcb6918cc37e51d938ab55a
jkittley <jkd3g11@soton.ac.uk>
2015-11-15 14:01:25 +0000

    Changes to css and bug fixes

--- frontend/js/bubbles.js
+++ frontend/js/bubbles.js
@@ -5,20 +5,23 @@ var bubble = (function() {
         height = 0,
         center = {x:0, y:0, point: [0,0] },
         padding = 6, // separation between nodes
-        baseRadius = 20;
-        maxRadius = 50;
+        baseRadius = 5,
+        maxRadius = 10,
+        focusRadius= 50;
 
     var circle = null,
-        svg = null,
-        force = null,
-        nodes = null,
-        focusNode =null;
+        svg=null,
+        force=null,
+        nodes=null,
+        focusNode=null,
+        search_element=null,
+        vis_div=null,
+        node_info=null;
 
     var n = 200, // total number of nodes
         m = 10; // number of distinct clusters
 
-    var offScreenGravPoints = [[-20,-20]]
-
+    // Called during the animate
     function tick(e) {
       circle
           .each(gravity(.2 * e.alpha))
@@ -64,18 +67,32 @@ var bubble = (function() {
       };
     }
 
-
-    var init = function(w,h) {
-      width = w;
-      height = h;
-      center.x = w/2;
-      center.y = h/2;
+    // Called to start the visualisation
+    var init = function(v, se, ni) {
+      // Set key elements
+      vis_div = v;
+      search_element = se;
+      node_info = ni;
+      // Show search
+      $(node_info).hide();
+      showSearch();
+      // Set width and height of SVG
+      width  = $(vis_div).width() * 2;
+      height = $(vis_div).height() * 2;
+      width  = Math.max(width, height);
+      height = Math.max(width, height);
+      // Store center x, y coords
+      center.x = width/2;
+      center.y = (height/2) - ($(search_element).height()/2);
       center.point = [center.x,center.y];
+      // Load nodes
       nodes = loadNodes();
-      dist = 40;
-      offScreenGravPoints = [[-dist,-dist],[center.x,-dist],[w+dist, -dist],[-dist,center.y],[w+dist,center.y],[-dist,h+dist],[center.x,h+dist],[w+dist,h+dist]];
+      // Add search listener
+      $(search_element+' .search-input').on("keyup", search_state_changed);
+      $(search_element+' .search-input').focus();
     }
 
+    // Once nodes are all loaded
     var onNodesLoad = function(n) {
       
       nodes = n; 
@@ -86,15 +103,15 @@ var bubble = (function() {
         .gravity(0)
         .charge(0)
         .on("tick", tick)
+        .on('end', function() { console.log('Force ended!'); })
         .start();
 
 
-      svg = d3.select("body")
+      svg = d3.select(vis_div)
         .append("svg")
           .attr("width", width)
           .attr("height", height);
         
-
       circle = svg.selectAll(".node")
         .data(nodes)
         .enter()
@@ -106,6 +123,7 @@ var bubble = (function() {
           .on('click', nodeClick);
     }
 
+    // Set the gravitational home of a node
     var setNodeGravityPoint = function(node, gPoint) {
         if (gPoint !== null && gPoint !== undefined) {
           node.cx = gPoint[0];
@@ -116,6 +134,7 @@ var bubble = (function() {
         
     }
 
+    // Load node data
     var loadNodes = function() {
       d3.json("js/testdata.json", function(loadedData) {
         var loadedData = testData();
@@ -141,88 +160,174 @@ var bubble = (function() {
       });
     }
     
+    // When a node is selected
     var nodeClick = function(n, i) {
-   
       // If the node clicked is the one currently focused then ignore
       if (focusNode !== null && focusNode === n) return
-
       // Deselect current node
       if (focusNode !== null) resetNode(focusNode);  
-                
       // Remove focus class from all nodes
       $(".node").attr("class","node");
+      // Start force dynamics
+      force.start();  
+      // Hide all nodes
       for (i in nodes) { if (nodes[i] !== n) hideNode(nodes[i]); }
-
       // Make selected node the new focus node
       $("#node_"+n.data.id).attr("class","node focus-node");
-      $("#node_"+n.data.id).attr("r",100);
-      n.radius = 100;
+      $("#node_"+n.data.id).attr("r", focusRadius);
+      n.radius = 80;
       n.x = width / 2;
       n.y = height / 2;
-      n.fixed = true;
-      focusNode = n;      
+      focusNode = n;   
 
-      // Bring chums back in
+      // Hide search and show info
+      showInfo();
+      
+      // Bring neighbours back in
       setTimeout(function(){ 
         // TODO: COnvert ot callback
-        for (i in n.data.chums) {
-            setNodeGravityPoint(nodes[i], center.point)
+        focusNode.fixed = true;
+        for (i in n.data.neighbours) {
+          orbitNode(nodes[i]);
         }
-      }, 3000);
+        // Restart force
+        force.start();  
+      }, 2000);
       
     }
 
+    // Make node fly off screen i.e. its hiden
     var hideNode = function(node) {
-        var rnd = Math.round(Math.random() * offScreenGravPoints.length);
-        setNodeGravityPoint(node, offScreenGravPoints[rnd]);
+        var rndPos = rndNodePosition(Math.max(width/3, height/3));
+        setNodeGravityPoint(node, [center.x + rndPos.o, center.y - rndPos.a]);
     }
 
+    // Function to set gravity of a node to be in orbit of focusNode
+    var orbitNode = function(node) {
+        console.log(node);
+        var rndPos = rndNodePosition(focusRadius + padding);
+        setNodeGravityPoint(node, [center.x + rndPos.o, center.y - rndPos.a]);
+    }
+
+    // Calculate a random position at a dist_from_center fron the center point
+    var rndNodePosition = function(dist_from_center) {
+        var t = Math.round(Math.random() * 359);
+        var o = parseInt( dist_from_center * Math.sin(t) );
+        var a = parseInt( dist_from_center * Math.cos(t) );
+        return { "o":o, "h":dist_from_center, "a":a, "t":t }
+    }
+
+    // Reset the size and gravity point of all nodes
     var resetAllNodes = function() {
-        focusNode - null;
+        resetNode(focusNode);
+        focusNode = null;
         for (i in nodes) { hideNode(nodes[i]); }
-        setTimeout(function(){ 
-          // TODO: COnvert ot callback
-          for (i in nodes) {
-              n = nodes[i];
-              resetNode(n);
-              setNodeGravityPoint(n, center.point)
-          }
-          force.start();
-      }, 5000);
+        force.start();  
+        for (i in nodes) {
+            n = nodes[i];
+            resetNode(n);
+            setNodeGravityPoint(n, center.point)
+        }
     }
 
+    // Return a node to its original size and make sure its free to move
     var resetNode = function(n) {
-      n.fixed = false;
-      n.radius = n.origRa;
-      $("#node_"+n.data.id)
-        .attr("class","node")
-        .attr("r",n.origRa);
+      if (n !==null && n !== undefined) {
+        n.fixed = false;
+        n.radius = n.origRa;
+        $("#node_"+n.data.id)
+          .attr("class","node")
+          .attr("r",n.origRa);
+      }
     }
 
+    // Reset state to that at load
     var reset = function() {
+      showSearch();
       resetAllNodes();
     }
 
+    // Generate test data
     var testData = function() {
-      var testNodes = [];
-      
-      for (var i=0; i<60; i++) {
+      var testNodes  = [];
+      var totalNodes = 200;
+      var max_pop = null;
+      var min_pop = null;
+
+      for (var i=0; i<totalNodes; i++) {
+        var fakeNames = "";
+        var popularity = parseInt(Math.random() * 100);
+        if (max_pop===null || popularity > max_pop) max_pop = popularity;
+        if (min_pop===null || popularity < min_pop) min_pop = popularity;
+        // Add some neighbours
+        var neighbours = [];
+        while (neighbours.length < parseInt(totalNodes*0.2)) {
+          neighbours.push( parseInt(Math.random() * totalNodes) );
+        }
+        // Add test node to list
         testNodes.push({
           "id": i,
-          "name": "Test "+i,
+          "name": rndstr(2),
           "pypi": "http://www.pychart.com/get_pypi_data/"+i,
-          "popularity": parseInt(Math.random() * 100),
-          "neighbours": [0,1,2] 
+          "popularity": popularity,
+          "neighbours": neighbours
         });
       };
+      // Return as JSON
       return { 
         "stats": {
-          "max_pop": 5000,
-          "min_pop": 50
+          "max_pop": max_pop,
+          "min_pop": min_pop
         },
         "nodes": testNodes,
       }
     }
+
+    // Create a random string
+    var rndstr = function(num_chars) {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for( var i=0; i < num_chars; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
+    }
+
+    // When the state of the search box changes, reprocess the found nodes
+    var search_state_changed = function() {
+      var text = $(search_element+' .search-input').val().toLowerCase();
+      if (text !=="") resetNode(focusNode);
+      force.start();
+      for (n in nodes) {
+        if (text !=="" && nodes[n].data.name.toLowerCase().indexOf(text)) {
+          hideNode(nodes[n]);
+        } else {
+          setNodeGravityPoint(nodes[n], center.point);
+        }
+      } 
+      force.start();
+    }
+
+    var showInfo = function() {
+      panelsAnimate(node_info, search_element);
+    }
+
+    var showSearch = function() {
+      panelsAnimate(search_element, node_info);
+    }
+
+    var panelsAnimate = function(in_elem, out_elem) {
+        $(out_elem)
+          .css( "z-index", 8888 )
+          .animate({ "bottom": "-="+$(out_elem).height()+"px" }, "slow", function() {
+              $(out_elem).hide();
+              $(in_elem)
+                .css( "bottom", -1 * $(in_elem).height() )
+                .css( "z-index", 9999 )
+                .show()
+                .animate({ "bottom": "+="+$(in_elem).height()+"px" }, "slow");
+          });   
+    }
+    
 
     // -----------------
     // Public API
